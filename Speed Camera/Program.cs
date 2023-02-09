6ccc24a8b1b56b748fbbacc2fb4ffd974be6dc65
Blazingninja123 <118309706+Blazingninja123@users.noreply.github.com>
2023-02-08 22:26:42 -0600

    Initial commit

---
+++ Speed Camera/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please Enter a Speed Limit: ");
+        var speedLimit = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please Enter the Speed of a Car: ");
+        var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+        if (carSpeed < speedLimit)
+            Console.WriteLine("Ok");
+        else
+        {
+            const int kmPerDemeritPoint = 5;
+            var demeritPoints = (carSpeed - speedLimit) / kmPerDemeritPoint;
+            if (demeritPoints > 12)
+                Console.WriteLine("License Suspended");
+            else
+                Console.WriteLine("Demerit points: " + demeritPoints);
+
+
+        }
+    }
+}
